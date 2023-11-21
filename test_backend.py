1a2991d1cd61ff3e1dc761b31f80bf90ce943c1d
Anish Singh Walia <loveeanish@gmail.com>
2023-11-21 16:49:58 +0530

    add the python code

---
+++ test_backend.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_addition():
+    assert 2 + 2 == 4
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 6 * 4 == 24
+
+def test_division():
+    assert 10 / 2 == 5
